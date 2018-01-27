619961916d063ae29ac246fca54a588bd0b98643
Glaedar <andrewpeters103@hotmail.co.uk>
2018-01-27 05:46:46 +0000

    working sonar
    press e to activate the sonar. This was FUCKING HELL to do ;'-(

---
+++ Sonar/Assets/Scripts/Glow.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glow : MonoBehaviour
+{
+
+    float glowDelay;
+    float delay;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            delay = glowDelay;
+        }
+
+        Behaviour halo = (Behaviour)GetComponent("Halo");
+        if (delay <= 0)
+        {
+            halo.enabled = false;
+        }
+
+        delay -= Time.deltaTime;
+        Debug.LogError("Delay: " + delay);
+
+    }
+
+    // void OnTriggerEnter(Collider other)
+    // {
+    //    
+    //     Behaviour halo = (Behaviour)GetComponent("Halo");
+    //    // halo.enabled = false;
+    //
+    //     if (other.gameObject.name == "Sphere")
+    //     {
+    //         
+    //
+    //         halo.enabled = true;
+    //     }
+    //     else
+    //     {
+    //         Debug.LogError("Sphere not colliding");
+    //
+    //     }
+    // }
+
+    void OnTriggerStay(Collider other)
+    {
+        Debug.LogError("still here inside you");
+        Behaviour halo = (Behaviour)GetComponent("Halo");
+        // halo.enabled = false;
+
+        if (other.gameObject.name == "Sphere")
+        {
+            halo.enabled = true;
+            glowDelay = 3;
+        }
+    }
+}
+//
+//  void OnTriggerExit(Collider other)
+//  {
+//        Debug.LogError("exited");
+//        Behaviour halo = (Behaviour)GetComponent("Halo");
+//        if (other.gameObject.name == "Sphere")
+//         {
+//             halo.enabled = false;
+//         }
+//        else
+//        {
+//            halo.enabled = true;
+// 
+//        }
+//    }
+// 
+//}
---
+++ Sonar/Assets/Scripts/Sonar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sonar : MonoBehaviour {
+
+    float sonarDelay = 3.0f;
+    float delay;
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            this.gameObject.GetComponent<Collider>().enabled = true;
+            //this.GetComponent<Light>().enabled = true;
+            //transform.light.
+            delay = sonarDelay;
+        }
+        if (delay <= 0)
+        {
+            this.gameObject.GetComponent<Collider>().enabled = false;
+        }
+        delay -= Time.deltaTime;
+        
+    }
+
+}
